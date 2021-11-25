6cd8290077289fd55420c250f30372dc8acfb48c
Luana Gomes <luana.gomes.mc@gmail.com>
2021-11-25 15:19:05 +0000

    Add HTML button onclick function Initial test of onclick functionality using console.log to verify
    Once tested function beerGrid() dynamically appends to HTML div beer_grid
    each beer element in the API
    
    Each grid contains an image, name, abv and a link to more information (to do)

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ function fetchData() {
 	.then((res) => res.json())
 	.then((data) =>{
 		beerData = data
-		console.log(beerData)
 	})
 	.catch((error) => {
 		console.log(`Error Fetching data : ${error}`)
@@ -12,3 +11,22 @@ function fetchData() {
 	})
 };
 fetchData();
+
+//Build a beer grid with data returned from fetch
+function beerGrid(){
+	var mainContainer = document.getElementById('beer_grid');
+
+	//create a grid for each beer in the API
+	for(var i = 0; i < beerData.length; i++){
+		var div = document.createElement('div');
+		div.classList.add('product_holder');
+		div.innerHTML = 
+		'<div class = "imgwrap"><img src = "'+beerData[i].image_url+'" alt = "'+beerData[i].name+'"></div>'+
+		'<div class = "text_title">'+beerData[i].name+'</div>'+
+		'<div class = "text_small"><div class="abv_text">'+beerData[i].abv+'</div>'+
+		'<a class = "info_text" href = "#'+beerData[i].name+'"> More Info </a></div><br>';
+
+		//append all grids created to the html page
+		mainContainer.append(div);
+	}
+}
